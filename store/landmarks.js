0c760054a23c75c8f67231c6dd5f58d4a06ea55c
a-Daley <daley.april@gmail.com>
2019-11-24 13:35:59 -0500

    Adds changes re: Rocky's comments

--- store/landmarks.js
+++ store/landmarks.js
@@ -1,39 +1,43 @@
-
 //Action Constants
-const GOT_LANDMARKS = "GOT_LANDMARKS"
-const THREW_ERROR = "THREW_ERROR"
+const GOT_LANDMARKS = "GOT_LANDMARKS";
+const THREW_ERROR = "THREW_ERROR";
 
 //Action Creators
-const gotLandmarks = landmarks => ({type: GOT_LANDMARKS, landmarks})
-const threwError = err => ({type: THREW_ERROR, err})
+const gotLandmarks = landmarks => ({ type: GOT_LANDMARKS, landmarks });
+const threwError = err => ({ type: ERROR_GETTING_LANDMARKS, err });
 
 //Thunks
-export const fetchLandmarks = (db) => async dispatch => {
+export const fetchLandmarks = db => async dispatch => {
   try {
-      db.once("value", snap => {
-        const data = [];
-        snap.forEach(child => {
-          const childObj = child.toJSON();
-          data.push({ ...childObj });
-        });
-        dispatch(gotLandmarks(data))
+    db.once("value", snap => {
+      const data = [];
+      snap.forEach(child => {
+        const childObj = child.toJSON();
+        data.push({ ...childObj });
       });
+      dispatch(gotLandmarks(data));
+    });
   } catch (error) {
-    console.log(error)
-    dispatch(threwError(error))
+    console.log(error);
+    dispatch(threwError(error));
   }
-}
-
+};
 
 //Sub-Reducer
-const initialState = {}
+const initialState = {
+  data: {},
+  err: false
+};
 
 const landmarks = (state = initialState, action) => {
-  switch(action.type) {
-    case GOT_LANDMARKS: return {...state, data: action.landmarks, err: false}
-    case THREW_ERROR: return {...state, err: true}
-    default: return state;
+  switch (action.type) {
+    case GOT_LANDMARKS:
+      return { ...state, data: action.landmarks, err: false };
+    case THREW_ERROR:
+      return { ...state, err: true };
+    default:
+      return state;
   }
-}
+};
 
 export default landmarks;
