56f7dd3d40be42a66bae7ed11660670ee1721dfa
Rachel Fine <rachelefine@gmail.com>
2019-11-24 17:40:58 -0500

    added ability to clear directions

--- store/directions.js
+++ store/directions.js
@@ -1,13 +1,14 @@
 import * as Location from "expo-location";
 import Constants from "../constants/APIKeys";
-import { decode } from '../util'
-
+import { decode } from "../util";
 
 //Action constants
-const SET_DIRECTIONS = "SET_DIRECTIONS"
+const SET_DIRECTIONS = "SET_DIRECTIONS";
+const CLEAR_DIRECTIONS = "CLEAR_DIRECTIONS";
 
 //Action creators
-const setDirections = directions => ({type: SET_DIRECTIONS, directions})
+const setDirections = directions => ({ type: SET_DIRECTIONS, directions });
+export const clearDirections = () => ({ type: CLEAR_DIRECTIONS });
 
 //Thunks
 export const getDirections = landmarkCoordinates => async dispatch => {
@@ -24,20 +25,24 @@ export const getDirections = landmarkCoordinates => async dispatch => {
   let data = await response.json();
   if (data.routes.length) {
     const coordinates = decode(data.routes[0].overview_polyline.points);
-    dispatch(setDirections({show: true, coordinates}))
+    dispatch(setDirections({ show: true, coordinates }));
   }
   // TODO: need some kind of error handling if no directions are available
   // TODO: close drawer when get directions (reset drawer height?)
 };
 
 //Sub-reducer
-const initialState = {}
+const initialState = {};
 
 const directions = (state = initialState, action) => {
-  switch(action.type) {
-    case SET_DIRECTIONS: return action.directions
-    default: return state
+  switch (action.type) {
+    case SET_DIRECTIONS:
+      return action.directions;
+    case CLEAR_DIRECTIONS:
+      return {};
+    default:
+      return state;
   }
-}
+};
 
-export default directions
+export default directions;
