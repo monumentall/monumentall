aa060dbb383a69b35ed37a10fba8916dfa8278a4
a-Daley <daley.april@gmail.com>
2020-01-05 14:18:17 -0500

    New envs

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
 import Constants from "expo-constants";
 import * as firebase from "firebase";
 
+const firebaseConfig = {
+  "apiKey": Constants.manifest.extra.firebaseConfig.apiKey,
+  "authDomain": Constants.manifest.extra.firebaseConfig.authDomain,
+  "databaseURL": Constants.manifest.extra.firebaseConfig.databaseURL,
+  "projectId": Constants.manifest.extra.firebaseConfig.projectId,
+  "storageBucket": Constants.manifest.extra.firebaseConfig.storageBucket,
+  "messagingSenderId": Constants.manifest.extra.firebaseConfig.messagingSenderId,
+  "appId": Constants.manifest.extra.firebaseConfig.appId
+};
+
 //Initialize firebase if not already initialized:
 if (!firebase.apps.length) {
-  firebase.initializeApp(Constants.manifest.extra.firebaseConfig);
-}
+  firebase.initializeApp(firebaseConfig);
+};
 
 //Turn on the below if you want DB logging...
 // firebase.database.enableLogging(true)
