31f2e1122ae2b2e3ad1906c49a6e72642476922d
Sara Parelhoff <sara.parelhoff@gmail.com>
2020-01-12 20:07:29 -0500

    MapMarkers

---
+++ components/RecenterBtn.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { TouchableOpacity, Image } from "react-native";
+import specificStyles from "../styles";
+import PropTypes from "prop-types";
+
+const RecenterButton = ({ enabled, centerMap }) => {
+  return enabled ? (
+    <TouchableOpacity
+      style={specificStyles.centerBtnContainer}
+      onPress={centerMap}
+    >
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={require("../assets/images/placeholder-map-icon.png")}
+      />
+    </TouchableOpacity>
+  ) : null;
+};
+
+export default RecenterButton
+
+
+RecenterButton.propTypes = {
+  enabled: PropTypes.bool,
+  centerMap: PropTypes.func
+}
