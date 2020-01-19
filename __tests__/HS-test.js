b1be6961680583857af92207ca010623da03d90c
a-Daley <daley.april@gmail.com>
2020-01-19 18:39:02 -0500

    Testing snapshot tests

---
+++ __tests__/HS-test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { HomeScreen } from "../screens/HomeScreen";
+
+jest.mock("../screens/Map", () => "Map");
+jest.mock("../screens/Explore", () => "Explore");
+
+describe("HomeScreen", () => {
+    it(`renders the map and the "Explore" drawer as the default`, () => {
+        const tree = renderer.create( <HomeScreen />).toJSON();
+        console.log(tree)
+        expect(tree).toMatchSnapshot();
+    });
+});
