d104ed9eee892141068f6383e397ccb50f0211f4
a-Daley <daley.april@gmail.com>
2020-01-19 14:02:42 -0500

    Updates db

--- db.js
+++ db.js
@@ -2,13 +2,13 @@ import Constants from "expo-constants";
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  "apiKey": Constants.manifest.extra.firebaseConfig.apiKey,
-  "authDomain": Constants.manifest.extra.firebaseConfig.authDomain,
-  "databaseURL": Constants.manifest.extra.firebaseConfig.databaseURL,
-  "projectId": Constants.manifest.extra.firebaseConfig.projectId,
-  "storageBucket": Constants.manifest.extra.firebaseConfig.storageBucket,
-  "messagingSenderId": Constants.manifest.extra.firebaseConfig.messagingSenderId,
-  "appId": Constants.manifest.extra.firebaseConfig.appId
+  "apiKey": process.env.FIREBASE_API_KEY,
+  "authDomain": process.env.FIREBASE_AUTH_DOMAIN,
+  "databaseURL": process.env.FIREBASE_DATABASE_URL,
+  "projectId": process.env.FIREBASE_PROJECT_ID,
+  "storageBucket": process.env.FIREBASE_STORAGE_BUCKET,
+  "messagingSenderId": process.env.FIREBASE_MESSAGING_SENDER_ID,
+  "appId": process.env.FIREBASE_APP_ID,
 };
 
 //Initialize firebase if not already initialized:
