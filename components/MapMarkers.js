b70c17ca5282f07b6148d6fee28c1392355853c0
Sara Parelhoff <sara.parelhoff@gmail.com>
2020-01-20 17:19:48 -0500

    Braces

--- components/MapMarkers.js
+++ components/MapMarkers.js
@@ -1,23 +1,23 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from "prop-types";
+import React from "react";
 import { Marker } from "react-native-maps";
 
 const MapMarkers = ({ markers, setRegionAndSelectLandmark }) => {
-  if (markers)
-  return markers.map(marker => (
-    <Marker
-    key={marker.name}
-    coordinate={marker.coordinate}
-    title={marker.name}
-    onPress={event => setRegionAndSelectLandmark(event, marker)}
-    />
+  if (markers) {
+    return markers.map(marker => (
+      <Marker
+        key={marker.name}
+        coordinate={marker.coordinate}
+        title={marker.name}
+        onPress={event => setRegionAndSelectLandmark(event, marker)}
+      />
     ));
-    return null;
-  };
+  } else return null;
+};
 
-  MapMarkers.propTypes = {
-    markers: PropTypes.array,
-    setRegionAndSelectLandmark: PropTypes.func
-  }
+MapMarkers.propTypes = {
+  markers: PropTypes.array,
+  setRegionAndSelectLandmark: PropTypes.func
+};
 
-export default MapMarkers
+export default MapMarkers;
